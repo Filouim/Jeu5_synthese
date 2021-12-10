9aa3941c926e7a21c11fc59c2a13f786ea51734e
Philippe Hubert <philou13938@gmail.com>
2021-12-10 16:45:48 -0500

    Créer les scènes pour la navigation du jeu ainsi que commencer l'écriture de son script

--- Assets/Script/ChangerScene.cs
+++ Assets/Script/ChangerScene.cs
@@ -5,8 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class ChangerScene : MonoBehaviour
 {
+    [SerializeField] private float _delai = 0.25f;
+
+    private static ChangerScene _instance;
+    public static ChangerScene instance => _instance;
+
     public void LoadScene(string nomScene)
     {
-        SceneManager.LoadScene(nomScene);
+        ListeScenes.instance.historiqueScenes.Add(SceneManager.GetActiveScene().name);
+        StartCoroutine(ChangeSceneDelay(nomScene, _delai, false, false));
+    }
+
+    public void LoadPreviousScene()
+    {
+        StartCoroutine(ChangeSceneDelay("", _delai, true, false));
+    }
+
+    public void LoadNextScene()
+    {
+        ListeScenes.instance.historiqueScenes.Add(SceneManager.GetActiveScene().name);
+        StartCoroutine(ChangeSceneDelay("", _delai, false, true));
+    }
+
+    public void QuitterJeu()
+    {
+        Application.Quit();
+    }
+
+    private IEnumerator ChangeSceneDelay(string nomScene, float delai, bool isPreviousScene, bool isNextScene)
+    {
+        yield return new WaitForSeconds(delai);
+
+        if (isPreviousScene && !isNextScene && nomScene == "") ListeScenes.instance.LoadPreviousScene();
+        else if (isNextScene && !isPreviousScene && nomScene == "") SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        else SceneManager.LoadScene(nomScene);
     }
 }
